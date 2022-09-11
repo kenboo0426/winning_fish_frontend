7143ec830a00d10f5cb1344a9b29a74a917331b1
kenboo0426 <kenshin.kenboo@gmail.com>
2022-09-12 00:39:46 +0900

    create user create and show api

---
+++ src/api/user.ts
@@ -0,0 +1,23 @@
+import { User } from '../interface';
+import { apiClient } from './apiClient';
+
+type UserCreateParams = {
+  uuid: string;
+  name: string;
+  email: string;
+};
+
+export const create = async (params: UserCreateParams) => {
+  const response = await apiClient.post('/user', params);
+
+  const data = response.data as User;
+  return data;
+};
+
+export const show = async (id: string) => {
+  const response = await apiClient.get(`/user/${id}`);
+
+  const data = response.data as User;
+  return data;
+};
+
